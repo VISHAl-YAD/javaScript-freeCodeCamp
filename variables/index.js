b5fe6fa955add1d6453407e8890b93533896f106
Vishal <vishal2004500@gmail.com>
2024-06-06 15:34:14 +0530

    Variable

---
+++ variables/index.js
@@ -0,0 +1,40 @@
+// Variable  =  A container that shows a value.
+//              Behaves as if were the value it contains.
+
+// 1. declaration : let x;
+// 2. assignment  : x = 100; 
+
+// String
+let myName = `Vishal`;
+let myfavFood = `Biryani` ;
+
+// Number / Integers
+let x;
+let y = 123;
+x = 100;
+console.log(x);
+console.log(y);
+let age = 25;
+let cgp = 6.97;
+
+// variable mix printing
+console.log(`My name is ${myName}`);
+console.log(`My age is ${age}.`);
+console.log(`My cgp score is ${cgp}.`);
+console.log(`My favourite food is ${myfavFood}.`);
+
+
+// Boolean 
+let online = true;
+console.log(`Vishal is online : ${online}`);
+
+
+// data type printing
+console.log(`data types of:`);
+console.log(typeof myName);
+console.log(typeof myfavFood);
+console.log(typeof age);
+console.log(typeof cgp);
+console.log(typeof online);
+
+
