cf7d0d3d10d000efca980108ff5e6815bed5bd4f
Vishal <vishal2004500@gmail.com>
2024-06-06 15:52:47 +0530

    Variable Example

---
+++ variables/Variable example/index.js
@@ -0,0 +1,7 @@
+let fullName = `Vishal Yadav`;
+let age = `19`;
+let isStudent = true;
+
+document.getElementById("p1").textContent = `My Name is ${fullName}` ;
+document.getElementById("p2").textContent = `My age is ${age}` ;
+document.getElementById("p3").textContent = `Student : ${isStudent}` ;
