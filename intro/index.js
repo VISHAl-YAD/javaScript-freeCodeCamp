baac89ce87113d87b3b717a822b2cdee401810d7
Vishal <vishal2004500@gmail.com>
2024-06-06 15:09:52 +0530

    Intro JS

---
+++ intro/index.js
@@ -0,0 +1,10 @@
+// console.log(`Hello World`);
+// console.log(`i like pizza!`);
+
+// ALERT MESSAGE
+// window.alert(`This Is An Alert!`);
+
+// HTML TEXT USING JAVASCRIPT
+
+document.getElementById("myH1").textContent = `VISHAL` ;
+document.getElementById("myP").textContent = `I like Pizza!` ;
