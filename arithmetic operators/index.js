a26f8266d6056a851b7b0ad32f99ca0fcda31abe
Vishal <vishal2004500@gmail.com>
2024-06-08 16:50:19 +0530

    arithmetic operators

---
+++ arithmetic operators/index.js
@@ -0,0 +1,24 @@
+
+document.getElementById("myh1").textContent = "Arithmetic Operatores";
+// Arithmetic Operators = operands (values, variable, etc.)
+//                      = operators (=, -, / , *, **, %)
+//                        ex: 11 = x + 1;
+
+// order of operations like BODMAS
+/*
+    1. parenthesis ()
+    2. exponents
+    3. multiplications / division / modulo
+    4. additions / subtractions
+*/
+
+
+
+let students = 43;
+students = students / 2;
+students += 6;
+students ++;
+let x = 2;
+x = x ** 2;
+console.log(`students : ${students}`);
+console.log(`x : ${x}`);
